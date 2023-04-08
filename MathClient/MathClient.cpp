fe5d7cb582b2ed2f0c374e0b26a5dc91cac1e75e
User <User@DESKTOP-AMU1QQO>
2023-04-08 17:46:28 +0700

    Добавьте файлы проекта.

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,20 @@
+﻿#include "MathLibrary.h"
+namespace MathLibrary
+{
+	double Arithmetic::Add(double a, double b)
+	{
+		return a + b;
+	}
+	double Arithmetic::Substruct(double a, double b)
+	{
+		return a - b;
+	}
+	double Arithmetic::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+	double Arithmetic::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
---
+++ MathClient/MathLibrary.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace MathLibrary
+{
+    class Arithmetic
+    {
+    public:
+        static double Add(double a, double b);
+        static double Substruct(double a, double b);
+        static double Multiply(double a, double b);
+        static double Divide(double a, double b);
+    };
+}
